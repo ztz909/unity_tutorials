9f5791866a13ad46073f7f9f6ea07de094243740
ztz909 <ztz909@gmail.com>
2023-10-30 10:34:02 +0200

    First commit and .gitignore

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player vehicle movement here
+        //transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
